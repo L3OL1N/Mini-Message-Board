ca567e4a38cc91ae9c6bd69a4bcce5a0ad19d300
L3OL1N <z3650888@gmail.com>
2022-10-18 12:49:06 +0800

    Add file

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+/* GET NEW page. */
+router.get('/new', function(req, res) {
+  res.render("form");
+});
+router.post('/new',function(req,res,next){
+  const messageText = req.body.textarea;
+  const messageUser = req.body.userName;
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+  console.log(messages[2])
+  res.redirect('/')
+})
+module.exports = router;
